236ae93c5bae793f972ab9c62f18f6714c4f4e06
mikael <40277714+m2wall@users.noreply.github.com>
2026-05-25 10:35:43 +0200

    started service-tests

--- src/test/java/org/example/backend1/Controller/CustomerControllerTest.java
+++ src/test/java/org/example/backend1/Controller/CustomerControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CustomerControllerTest extends BaseControllerTest {
@@ -36,21 +35,39 @@ class CustomerControllerTest extends BaseControllerTest {
                 .exchange()
                 .expectStatus().isOk();
         List<Customer> all = customerRepository.findAll();
+
+        long count = all.stream().
+                filter(customer -> customer.getName().equals("Testsson"))
+                .count();
+
+        assertEquals(1, count);
         assertTrue(all.getLast().getName().equals("Testsson"));
     }
 
 
     //TODO: [TEST] Dunno if fixed and broken or never finished it.
     @Test
+    //TODO [TEST] Test 400 if customer has booking and 200 if no booking
     void deleteByIdTest() {
         List<Customer> customers = customerRepository.findAll();
-        Long idBeforeDelete = customers.get(0).getId();
+        Customer customer = customers.get(0);
+
         restTestClient.delete()
-                .uri("http://localhost:8080/customers/delete/" + idBeforeDelete)
+                .uri("http://localhost:8080/customers/delete/" + customer.getId())
+                .exchange()
+                .expectStatus().isEqualTo(400);
+
+        customers = customerRepository.findAll();
+
+        assertTrue(customers.contains(customer));
+
+
+        restTestClient.delete()
+                .uri("http://localhost:8080/customers/delete/" + customer.getId())
                 .exchange()
                 .expectStatus().isOk();
 
         customers = customerRepository.findAll();
-        assertFalse(customers.contains(idBeforeDelete));
+        assertFalse(customers.contains(customer));
     }
 }
